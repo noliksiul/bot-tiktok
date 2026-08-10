23c41194e9ed9616695cc3046707ef49da047ebc
noliksiul <79671287+noliksiul@users.noreply.github.com>
2026-08-09 20:10:57 -0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ from flask import Flask
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
 from telegram.ext import Application, ContextTypes, CommandHandler
 
-TOKEN = "TU_TOKEN_AQUI"
-DATABASE_URL = "TU_DATABASE_URL_AQUI"
+# Datos reales
+TOKEN = "6564290496:AAFfyjhNUHMQaryJgMxK-gBNGkJX41Cay0A"
+DATABASE_URL = "postgresql://bot_db1_user:B2y3STMCDTW1HB7adfk2TBYzB10GyaAL@dpg-d9sfnlu7bikc739fl5gg-a.oregon-postgres.render.com/bot_db1?sslmode=require"
 
 logging.basicConfig(level=logging.INFO)
 
@@ -66,7 +67,7 @@ async def main():
     await init_db()
     await application.initialize()
     await application.start()
-    await application.run_polling()   # <-- solo polling
+    await application.run_polling()   # <-- solo polling, sin webhook
 
 # Arrancar el bot en un hilo separado para no bloquear Flask
 
